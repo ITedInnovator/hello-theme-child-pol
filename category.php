04087067228af738bc7170dc07cecd53723d5b7f
Anthony Church <ag.church21@gmail.com>
2024-10-15 20:53:58 +0100

    Created initial card layout for category archive page.

---
+++ category.php
@@ -0,0 +1,17 @@
+<?php
+get_header(); ?>
+<main class="category-page">
+    <h1 class="category-title"><?php single_cat_title(); ?></h1>
+<div class="container">
+<?php if(have_posts()) :
+    while(have_posts()) : the_post();?>
+    <div class="post elementor-post__card">
+        <?php the_post_thumbnail('medium') ?> 
+        <a href="<?php the_permalink(); ?>"><h2 class="blog-title"><?php the_title(); ?></h2></a>
+        <p><?php the_excerpt() ?></p>
+        </div>
+   <?php endwhile;
+endif;
+?>
+</div>
+</main>
